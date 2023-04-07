d471a14d9f84731a12c6e54b825fdef8073823ac
ME Accounts <sa-forest@gatech.edu>
2023-04-07 18:59:51 -0400

    initial square wave triggering work

--- holypipette/devices/amplifier/DAQ.py
+++ holypipette/devices/amplifier/DAQ.py
@@ -14,16 +14,20 @@ class DAQ:
 
         self.readChannel = readChannel
         self.cmdChannel = cmdChannel
-        self.numSamples = 2000
+
+        #read constants
+        self.samplesPerSec = 5000
+        self.readTime = 0.2
+        self.numSamples = int(self.samplesPerSec * self.readTime)
 
         print(f'Using {self.readDev}/{self.readChannel} for reading and {self.cmdDev}/{self.cmdChannel} for writing.')
 
     def _readAnalogInput(self):
         with nidaqmx.Task() as task:
             task.ai_channels.add_ai_voltage_chan(f'{self.readDev}/{self.readChannel}', max_val=10, min_val=0)
-            task.timing.cfg_samp_clk_timing(20000, sample_mode=nidaqmx.constants.AcquisitionType.FINITE, samps_per_chan=self.numSamples)
-            task.triggers.reference_trigger.cfg_anlg_edge_ref_trig(f'{self.readDev}/{self.readChannel}', pretrigger_samples = 10, trigger_slope=nidaqmx.constants.Slope.RISING, trigger_level = 0.1 )
-            data = task.read(number_of_samples_per_channel=2000)
+            task.timing.cfg_samp_clk_timing(self.samplesPerSec, sample_mode=nidaqmx.constants.AcquisitionType.FINITE, samps_per_chan=self.numSamples)
+            # task.triggers.reference_trigger.cfg_anlg_edge_ref_trig(f'{self.readDev}/{self.readChannel}', pretrigger_samples = 10, trigger_slope=nidaqmx.constants.Slope.RISING, trigger_level=0.2)
+            data = task.read(number_of_samples_per_channel=self.numSamples, timeout=10)
             data = np.array(data, dtype=float)
             task.stop()
 
@@ -42,21 +46,21 @@ class DAQ:
         task.timing.cfg_samp_clk_timing(samplesPerSec, sample_mode=nidaqmx.constants.AcquisitionType.CONTINUOUS)
         
         #create a wave_freq Hz square wave
-        data = np.zeros(int(samplesPerSec / recordingTime))
-        onTime = 1 / wave_freq * dutyCycle * samplesPerSec
-        offTime = 1 / wave_freq * (1-dutyCycle) * samplesPerSec
+        data = np.zeros(int(samplesPerSec * recordingTime))
+        onTime = (1 / wave_freq) * dutyCycle * samplesPerSec #in samples
+        offTime = (1 / wave_freq) * (1-dutyCycle) * samplesPerSec
 
         #calc period
-        period = onTime + offTime
+        period = onTime + offTime #in samples
 
         #convert to int
         onTime = int(onTime)
         offTime = int(offTime)
         period = int(period)
 
-        wavesPerSec = samplesPerSec // period
+        numWaves = int(data.shape[0] / period)
 
-        for i in range(wavesPerSec):
+        for i in range(numWaves):
             data[i * period : i * period + onTime] = amplitude
 
         task.write(data)
@@ -70,7 +74,7 @@ class DAQ:
         sendTask.stop()
         sendTask.close()
 
-        xdata = np.linspace(0, samplesPerSec * recordingTime, self.numSamples, dtype=float)
+        xdata = np.linspace(0, recordingTime, self.numSamples, dtype=float)
 
         return np.array([xdata, data])
 
